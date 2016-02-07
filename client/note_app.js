05ebac93b6221a140818fa2cb8fca8931cc526e2
John Shrader <figurefactory@gmail.com>
2016-02-07 02:36:50 -0500

    mce editor changes

--- client/note_app.js
+++ client/note_app.js
@@ -5,16 +5,42 @@ $.getScript('http://cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.js', function(d
     $(document).ready(function()
     {
         tinymce.init({
-    selector: '#mytextarea'
+    selector: '.mce',
+     plugins: ['advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste'],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
+    media_live_embeds: true,
+    browser_spellcheck: true,
+    inline: true
   });
   
+         tinymce.init({
+    selector: '#mytextarea',
+     plugins: ['advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste'],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
+    media_live_embeds: true,
+    browser_spellcheck: true
+  });
+  
+  tinymce.init({
+  selector: 'h3.edit',
+  inline: true,
+  toolbar: false,
+  menubar: false
+});
+
+  
    $('#one').click(function(){
-    console.log(tinyMCE.activeEditor.getContent());
+    console.log(tinyMCE.activeEditor.getContent()+$('#title').val());
    noteInfo = {
-        title : "Title",
+        title : $('#title').val(),
         content : tinyMCE.activeEditor.getContent()
     }; 
     Notes.insert( noteInfo);
+    
  });
 
         // MAKE SURE YOUR SELECTOR MATCHES SOMETHING IN YOUR HTML!!!
