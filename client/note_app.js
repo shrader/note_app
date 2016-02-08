680c3dd3653aa304748c8e74b70e0f96bae618f7
John Shrader <figurefactory@gmail.com>
2016-02-08 14:32:38 -0500

    Revert "Its all working"
    This reverts commit ca806f01bffbdd6687cccff60b8cdc094bb16eff.

--- client/note_app.js
+++ client/note_app.js
@@ -1,4 +1,53 @@
+
+$.getScript('http://cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.js', function(data, textStatus){
+	console.log(textStatus);
+    // Create the tooltips only when document ready
+    $(document).ready(function()
+    {
+        tinymce.init({
+    selector: '.mce',
+     plugins: ['advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste'],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
+    media_live_embeds: true,
+    browser_spellcheck: true,
+    inline: true
+  });
   
+         tinymce.init({
+    selector: '#mytextarea',
+     plugins: ['advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste'],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
+    media_live_embeds: true,
+    browser_spellcheck: true
+  });
+  
+  tinymce.init({
+  selector: 'h3.edit',
+  inline: true,
+  toolbar: false,
+  menubar: false
+});
+
+  
+   $('#one').click(function(){
+    console.log(tinyMCE.activeEditor.getContent()+$('#title').val());
+   noteInfo = {
+        title : $('#title').val(),
+        content : tinyMCE.activeEditor.getContent()
+    }; 
+    Notes.insert( noteInfo);
+    
+ });
+
+
+    });
+        
+});
+$(document).ready(function() {
    $('#one').click(function(){
     console.log(tinyMCE.activeEditor.getContent()+$('#title').val());
    noteInfo = {
@@ -15,19 +64,4 @@
     console.log(noteInfo);
     
  });
-
-$(document).ready(function()
-    {
-        
-        		tinymce.init({
-		selector: '#mytextarea',
-		plugins: ['advlist autolink lists link image charmap print preview anchor',
-		'searchreplace visualblocks code fullscreen',
-		'insertdatetime media table contextmenu paste'],
-		toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
-		media_live_embeds: true,
-		browser_spellcheck: true
-	});
-   
-
-    });
+}); 
