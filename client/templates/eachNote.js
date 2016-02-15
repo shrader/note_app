d9405e3ceb1eb69bc155785738ed63476f2bdea6
John Shrader <figurefactory@gmail.com>
2016-02-15 18:39:07 -0500

    edit & delete are hidden by CSS not JS now

--- client/templates/eachNote.js
+++ client/templates/eachNote.js
@@ -1,16 +1,6 @@
 
 Template.eachNote.onRendered(function(){
-		
-//edit and delete buttons hidden by default	
-    $('.noteBtns').hide();	
-		
-//  only show edit and delete buttons when hovering on that note 
-    $(document).on('mouseenter', '.notes', function () {
-        $(this).find(".noteBtns").show();
-    }).on('mouseleave', '.notes', function () {
-        $(this).find(".noteBtns").hide();
-   		 });
-    
+		    
 });
 
 
