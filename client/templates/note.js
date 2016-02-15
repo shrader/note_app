3713c9048b4cab8af1fb0c37cd83c9304ea22c1e
John Shrader <figurefactory@gmail.com>
2016-02-15 00:02:28 -0500

    added home button to side menu

--- client/templates/note.js
+++ client/templates/note.js
@@ -64,26 +64,4 @@ Template.note.events({
   }
 });
 
-/* $(document).ready(function() {
-  
-         tinymce.init({
-    selector: '.mytextarea',
-     plugins: ['advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table contextmenu paste'],
-    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
-    media_live_embeds: true,
-    browser_spellcheck: true
-    });
-    
-    
-    $(document).on('focusin', function(e) {
-    if ($(e.target).closest(".mce-window, .moxman-window").length) {
-		e.stopImmediatePropagation();
-	}
-    });
-    
-    
-  
-   }); */
    
