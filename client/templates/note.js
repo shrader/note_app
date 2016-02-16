e9c4ed2029f2bd7384c815136b35cd70b6454d32
John Shrader <figurefactory@gmail.com>
2016-02-16 13:11:10 -0500

    added styles for tooltips, added tooltip links to notes, made add/edit note modals draggable

--- client/templates/note.js
+++ client/templates/note.js
@@ -64,4 +64,59 @@ Template.note.events({
   }
 });
 
-   
+Template.note.onRendered(function () {
+
+    makeDraggable;
+
+    $(".editModal").drags();
+});
+
+var makeDraggable = (function($) {
+    $.fn.drags = function(opt) {
+
+        opt = $.extend({handle:"",cursor:"move"}, opt);
+
+        if(opt.handle === "") {
+            var $el = this;
+        } else {
+            var $el = this.find(opt.handle);
+        }
+
+        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+            if(opt.handle === "") {
+                var $drag = $(this).addClass('draggable');
+            } else {
+                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+            }
+            var z_idx = $drag.css('z-index'),
+                drg_h = $drag.outerHeight(),
+                drg_w = $drag.outerWidth(),
+                pos_y = $drag.offset().top + drg_h - e.pageY,
+                pos_x = $drag.offset().left + drg_w - e.pageX;
+                $(window).one('mouseup', function(e) {
+                    if (opt.handle === '') {
+                        $drag.removeClass('draggable');
+                    } else {
+                        $drag.removeClass('active-handle').parent().removeClass('draggable');
+                    }
+                    $(window).unbind('mouseup');
+                    $drag.parents().unbind('mousemove');
+                });
+            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+                $('.draggable').offset({
+                    top:e.pageY + pos_y - drg_h,
+                    left:e.pageX + pos_x - drg_w
+                }).on("mouseup", function() {
+                    $(this).removeClass('draggable').css('z-index', z_idx);
+                });
+            });
+        }).on("mouseup", function() {
+            if(opt.handle === "") {
+                $(this).removeClass('draggable');
+            } else {
+                $(this).removeClass('active-handle').parent().removeClass('draggable');
+            }
+        });
+
+    }
+})(jQuery);
